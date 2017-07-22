e9bb9eb745b0d0e7fe53c8a5257b4494c8588888
wknar <wakana.ara@gmail.com>
2017-07-22 11:56:03 +0900

    add podfile

---
+++ CocktailParty/CocktailListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CocktailListViewController.swift
+//  CocktailParty
+//
+//  Created by アラ 若菜 on 2017/05/01.
+//  Copyright © 2017 wknar. All rights reserved.
+//
+
+import UIKit
+
+class CocktailListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+target 'CocktailParty' do
+  use_frameworks!
+
+  pod 'RealmSwift'
+
+  target 'CocktailPartyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CocktailPartyUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
