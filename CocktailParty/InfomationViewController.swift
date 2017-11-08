076664439a32bf44b2f745edbb7c1bdf16e6aca7
wknar <wakana.ara@gmail.com>
2017-11-08 16:31:19 +0900

    add new registeration view controller

---
+++ CocktailParty/InfomationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InfomationViewController.swift
+//  CocktailParty
+//
+//  Created by アラ 若菜 on 2017/05/01.
+//  Copyright © 2017 wknar. All rights reserved.
+//
+
+import UIKit
+
+class InfomationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "App Info"
+    }
+
+}
+
---
+++ CocktailParty/NewCocktailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewCocktailViewController.swift
+//  CocktailParty
+//
+//  Created by wknar on 2017/11/08.
+//  Copyright © 2017 wknar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewCocktailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+
+}
