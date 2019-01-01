d7ec35f0b9235df03e4fde72d043d13a3e70f253
wknar <wakana.ara@gmail.com>
2019-01-01 22:03:19 +0900

    add new title VC

---
+++ CocktailParty/ViewControllers/NewTitleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewTitleViewController.swift
+//  CocktailParty
+//
+//  Created by Wakana Ara on 2019/01/01.
+//  Copyright © 2019 wknar. All rights reserved.
+//
+
+import UIKit
+
+class NewTitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
