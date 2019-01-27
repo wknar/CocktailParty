26aef17ed189d51e55963ffc3883ed388d336144
wknar <wakana.ara@gmail.com>
2019-01-27 19:23:37 +0900

    add new post methods (title, portion)

--- CocktailParty/ViewControllers/NewPost/NewPortionViewController.swift
+++ CocktailParty/ViewControllers/NewPost/NewPortionViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import RealmSwift
 
-class NewPortionViewController: UIViewController {
+class NewPortionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var portionTableView: UITableView!
     @IBOutlet var nextButton: UIButton!
+
+    private var cocktail: Cocktail!
+
+    func setup(cocktail: Cocktail) {
+        self.cocktail = cocktail
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "New Post 2/4"
+
+        portionTableView.delegate = self
+
+        nextButton.addTarget(self, action: #selector(self.nextButtonTapped), for: .touchUpInside)
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       return UITableViewCell()
+    }
+
+    @objc func nextButtonTapped() {
+        let quantity = Quantity()
+        quantity.name = ""
+        quantity.amount = 0.0
+        cocktail.content = List([Quantity(name: "opo"), Quantity(name: "hoh")])
+    }
+
 }
--- CocktailParty/ViewControllers/NewPost/NewTitleViewController.swift
+++ CocktailParty/ViewControllers/NewPost/NewTitleViewController.swift
@@ -14,10 +14,34 @@ class NewTitleViewController: UIViewController {
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var nextButton: UIButton!
 
+    private var cocktail: Cocktail!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "New Post 1/4"
+
+        cocktail = Cocktail()
+
+        nextButton.addTarget(self, action: #selector(self.nextButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func nextButtonTapped() {
+        if setTitle() {
+            let vc = UIStoryboard(name: "NewPortion", bundle: nil).instantiateInitialViewController() as! NewPortionViewController
+            vc.setup(cocktail: cocktail)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+    func setTitle() -> Bool {
+        // TODO: trim spaces(new lines)
+        if let text = titleTextField.text, text.isEmpty  {
+            // TODO: show error message
+            return false
+        }
+        cocktail.title = titleTextField.text!
+        return true
     }
 
 }
