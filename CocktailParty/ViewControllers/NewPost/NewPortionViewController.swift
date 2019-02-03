93c7f5ff140581080a82471b7b2de89f376b12f0
wknar <wakana.ara@gmail.com>
2019-02-03 14:23:50 +0900

    add tableview delegate for portion view

--- CocktailParty/ViewControllers/NewPost/NewPortionViewController.swift
+++ CocktailParty/ViewControllers/NewPost/NewPortionViewController.swift
@@ -11,10 +11,16 @@ import RealmSwift
 
 class NewPortionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    enum Section: Int, CaseIterable {
+        case portions
+        case new
+    }
+
     @IBOutlet var portionTableView: UITableView!
     @IBOutlet var nextButton: UIButton!
 
     private var cocktail: Cocktail!
+    private var quantities = [Quantity]()
 
     func setup(cocktail: Cocktail) {
         self.cocktail = cocktail
@@ -26,16 +32,40 @@ class NewPortionViewController: UIViewController, UITableViewDelegate, UITableVi
         title = "New Post 2/4"
 
         portionTableView.delegate = self
+        portionTableView.dataSource = self
 
         nextButton.addTarget(self, action: #selector(self.nextButtonTapped), for: .touchUpInside)
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.allCases.count
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        guard let sec = Section(rawValue: section) else {
+            return 0
+        }
+        switch sec {
+        case .portions:
+            return quantities.count
+        case .new:
+            return 1
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return UITableViewCell()
+        guard let section = Section(rawValue: indexPath.section) else {
+            return UITableViewCell()
+        }
+        switch section {
+        case .portions:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NewPortionName", for: indexPath) as! NewPortionNameCell
+            cell.setup(name: quantities[indexPath.row].name)
+            return cell
+        case .new:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddNewPortion", for: indexPath) as! AddNewPortionCell
+            return cell
+        }
     }
 
     @objc func nextButtonTapped() {
@@ -44,5 +74,19 @@ class NewPortionViewController: UIViewController, UITableViewDelegate, UITableVi
         quantity.amount = 0.0
         cocktail.content = List([Quantity(name: "opo"), Quantity(name: "hoh")])
     }
+
+}
+
+class NewPortionNameCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+
+    func setup(name: String) {
+        self.nameLabel.text = name
+    }
+
+}
+
+class AddNewPortionCell: UITableViewCell {
 
 }
