64cc8a0939c38dd840dc2828201a91091a396fa0
wknar <wakana.ara@gmail.com>
2019-02-17 22:23:06 +0900

    add save function

--- CocktailParty/ViewControllers/NewPost/NewRateViewController.swift
+++ CocktailParty/ViewControllers/NewPost/NewRateViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Cosmos
+import RealmSwift
 
 class NewRateViewController: UIViewController {
 
@@ -31,5 +32,19 @@ class NewRateViewController: UIViewController {
 
     @objc func resistButtonTapped() {
         cocktail.rate = Int(self.starView.rating)
+        saveCocktail()
+    }
+
+    func saveCocktail() {
+        let realm = try! Realm()
+        // GET number of cocktail list
+        let cocktailNum = realm.objects(Cocktail.self).map{$0.id}.max() ?? 0
+        cocktail.id = cocktailNum + 1
+        try! realm.write() {
+            realm.add(cocktail)
+        }
+
+        // TODO: show alert after complete save
+        dismiss(animated: true, completion: nil)
     }
 }
